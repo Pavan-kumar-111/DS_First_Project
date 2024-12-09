459cd6bcc849bc01007befd467c79d095a15587a
Sri Adilakshmi Adapa <69632486+SRIADILAKSHMIADAPA@users.noreply.github.com>
2024-12-09 12:23:08 +0530

    Add files via upload

---
+++ Theme.js
@@ -0,0 +1,34 @@
+//import React from 'react';
+
+
+
+const theme = {
+  PRIMARY_BLUE: '#1d89cc',
+  ICE_BLUE: '#F0F8FF',
+  BRIGHT_BLUE: 'rgba(15,161,255,1)',
+  LIGHT_BLUE: 'rgba(121,202,255,1)',
+  DARK_BLUE: 'rgba(0, 65,107,1)',
+  NAVY_BLUE: 'rgba(0, 84,139,1)',
+  DULL_BLUE: 'rgba(169,221,255,1)',
+  MUTED_BLUE: 'rgba(13, 42, 77, 0.7)',
+  MUTED_BORDER: 'rgba(13, 42, 77, 0.15)',
+  DARK_COLOR: '#0d2a4d',
+  LIGHT_COLOR: '#FFFAFA',
+  MUTED_COLOR: '#dcdcdc',
+  GREEN_COLOR: 'green',
+  DEFAULT_FONT: 'HopeRegular',
+  BOLD_FONT: 'HopeBold',
+  SEMI_BOLD_FONT: 'HopeSemiBold',
+  ITALIC_FONT: 'HopeItalic',
+  SEMI_BOLD_ITALIC_FONT: 'HopeSemiBoldItalic',
+  BOLD_ITALIC_FONT: 'HopeBoldItalic',
+  RUPEE: '₹',
+  WARNING: '#ffc400',
+  ERROR: '#f44336',
+  MIN_HEIGHT: '100vh',
+  WIDTH: 'min(100vw, 425px)',
+  BREAK: '%0D%0A',
+  SHADOW: 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
+};
+
+export default theme;
